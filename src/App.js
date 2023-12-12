36063bba3b50371f179327e7d1254060380c4385
Anas-Kharboutli <127590238+Anas-Kharboutli@users.noreply.github.com>
2023-12-12 21:02:33 +0800

    change01

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import NavBar from './components/navBar';
-import {PageRouting} from './components/context';
-
+import NavBar           from './components/navBar';
+import {PageRouting}    from './components/context';
 import { ShareContext } from './components/context';
 
 function App() {
   return (
     <div className='main'>
-   
     <ShareContext>
-
       <NavBar/>
       <PageRouting/>
-      
     </ShareContext>
-   
     </div>
   );
 }
